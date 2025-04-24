fd96ae18e0cdbd1bb73b4185fcaa4d06e8fb7a07
AdilIqbal98 <adil.q@ayassbioscience.com>
2025-04-24 22:06:08 +0500

    validation_agent.py

--- validation_agent.py
+++ validation_agent.py
@@ -1,12 +1,14 @@
 import logging
-from schemas import LiteratureOutput, BiomarkerOutput, PathwayOutput
 from pydantic import ValidationError
+from schemas import LiteratureOutput, BiomarkerOutput, PathwayOutput
 
 class ValidationAgent:
 
     @staticmethod
     def validate_literature(data):
         try:
+            if isinstance(data, LiteratureOutput):
+                return True
             LiteratureOutput(**data)
             logging.info("[VALIDATOR] ✅ Literature passed")
             return True
@@ -17,6 +19,8 @@ class ValidationAgent:
     @staticmethod
     def validate_biomarkers(data):
         try:
+            if isinstance(data, BiomarkerOutput):
+                return True
             BiomarkerOutput(**data)
             logging.info("[VALIDATOR] ✅ Biomarkers passed")
             return True
@@ -27,6 +31,8 @@ class ValidationAgent:
     @staticmethod
     def validate_pathway(data):
         try:
+            if isinstance(data, PathwayOutput):
+                return True
             PathwayOutput(**data)
             logging.info("[VALIDATOR] ✅ Pathway passed")
             return True
